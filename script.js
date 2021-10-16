bf8b9b6a6be3bc2af94f10fae8455f3a415a6511
KJ <irateboolean@gmx.com>
2021-10-16 18:03:41 -0400

    added X and O on click

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const board = document.querySelector(".board")
+const board = document.querySelector(".gameboard")
+
+const gameBoard =(() => {
+  let player = 0
+
+  let changeClass = (square) => {
+    square.classList.add(player % 2 ? 'cross' : 'circle')
+    square.classList.remove(player % 2 ? 'circle' : 'cross')
+    player++
+    console.log(player)
+  }
+
+  return {changeClass}
+})()
+
+
 
 for (i = 0; i < 9; i++) {
   const square = document.createElement("div")
   square.setAttribute("class", "square")
+  square.setAttribute("onclick", "gameBoard.changeClass(this)")
   board.appendChild(square)
 }
