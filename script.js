4762e2ad00ebc80627c31208ec9353762a1b1ba2
KJ <irateboolean@gmx.com>
2021-10-16 19:21:37 -0400

    cannot overwrite selected squares

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const board = document.querySelector(".gameboard")
 
 const gameBoard =(() => {
-  let player = 0
+  let player = 1
+  let played = []
+
+
 
   let changeClass = (square) => {
+    if (!(played.includes(square.dataset.index))){
     square.classList.add(player % 2 ? 'cross' : 'circle')
     square.classList.remove(player % 2 ? 'circle' : 'cross')
+    played.push(square.dataset.index)
     player++
-    console.log(player)
+    }
   }
 
   return {changeClass}
@@ -19,5 +24,6 @@ for (i = 0; i < 9; i++) {
   const square = document.createElement("div")
   square.setAttribute("class", "square")
   square.setAttribute("onclick", "gameBoard.changeClass(this)")
+  square.dataset.index = i
   board.appendChild(square)
 }
