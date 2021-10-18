41ba1722980efa9a1baa61962551ff8dbcde9196
KJ <irateboolean@gmx.com>
2021-10-17 22:25:45 -0400

    added win check

--- script.js
+++ script.js
@@ -6,27 +6,52 @@ const gameBoard =(() => {
   let played = []
   let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+  let checkRow = (row) => {
+    if (row[0] == row[1] && row[1] == row[2])
+    {return true}
+    return false
+  }
+
+
   let changeClass = (square) => {
-    if (!(played.includes(square.dataset.index))){
+    if (!(played.includes(square.dataset.index)) && !(checkBoard())){
     const img = square.childNodes[0]
-    img.src = player % 2 == 0 ? 'cross.svg' : 'circle.svg'
-    img.classList.add(player % 2 == 0 ? "red" : "blue")
+    img.src = player % 2 == 0 ? 'cross.svg' : 'circle.svg';
+    img.classList.add(player % 2 == 0 ? "red" : "blue");
     played.push(square.dataset.index)
     board[square.dataset.index] = player % 2 == 0 ? "X" : "O"
     player++
-    console.log(board)
+    console.log([checkBoard(), board])
     }
   }
 
   let reset = () => {
     played = []
     player = 0
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     const squares = document.querySelectorAll(".square")
     for (i=0; i<squares.length; i++){
       squares[i].childNodes[0].src ='blank.svg'
+      squares[i].childNodes[0].setAttribute("class", "")
     }
   }
-  return {changeClass, reset}
+
+  let checkBoard = () => {
+    if (
+    checkRow(board.slice(0,3)) ||
+    checkRow(board.slice(3,6)) ||
+    checkRow(board.slice(6,9)) ||
+    checkRow([board[0], board[4], board[8]]) ||
+    checkRow([board[2], board[4], board[6]]) ||
+    checkRow([board[0], board[3], board[6]]) ||
+    checkRow([board[1], board[4], board[7]]) ||
+    checkRow([board[2], board[5], board[8]]) ){
+      return true
+    }
+    return false
+  }
+
+  return {changeClass, reset, checkBoard}
 })()
 
 resetButton.setAttribute("onclick", "gameBoard.reset()")
